c05501d0660644ca1675ed9365edcae12e868c95
Weng Xuetian <wengxt@gmail.com>
2014-04-12 12:12:16 -0400

    now it suprisingly works! by render to a widget instead of image.

--- helper/main.cpp
+++ helper/main.cpp
@@ -2,8 +2,10 @@
 
 #include <QApplication>
 #include <QPainter>
+#include <QPaintEvent>
 #include <QImage>
 #include <QImageWriter>
+#include <QWidget>
 
 #include <KComponentData>
 #include <KGlobal>
@@ -14,6 +16,35 @@
 #include <KGlobalSettings>
 
 #define MARGIN 3
+
+class PaintWidget: public QWidget
+{
+    Q_OBJECT
+    QString text;
+    QSize s;
+public:
+    PaintWidget() : QWidget(0){
+
+        text = i18n("The quick brown fox jumps over the lazy dog\n"
+            "The quick brown fox jumps over the lazy dog\n"
+            "The quick brown fox jumps over the lazy dog\n"
+            "The quick brown fox jumps over the lazy dog"
+        );
+        QFontMetrics fm(qApp->font());
+        s = fm.size(0, text);
+        resize(s + QSize(MARGIN * 2, MARGIN * 2));
+    }
+
+protected:
+    virtual void paintEvent(QPaintEvent*) {
+        QPainter painter(this);
+
+        painter.fillRect(rect(), qApp->palette().color(QPalette::Base));
+        painter.setFont(qApp->font());
+        painter.setPen(qApp->palette().color(QPalette::Text));
+        painter.drawText(QRect(QPoint(MARGIN, MARGIN), s), Qt::AlignCenter, text);
+    }
+};
 
 int main(int argc, char* argv[])
 {
@@ -26,27 +57,15 @@ int main(int argc, char* argv[])
         fprintf(stderr, "Usage: fontadjust_helper name\n");
         exit(1);
     }
-    
-    QString text = i18n("The quick brown fox jumps over the lazy dog\n"
-        "The quick brown fox jumps over the lazy dog\n"
-        "The quick brown fox jumps over the lazy dog\n"
-        "The quick brown fox jumps over the lazy dog"
-    );
-    //QFont font = KGlobalSettings::generalFont();
-    QFont font("AR PL UKai CN");
-    QFontMetrics fm(font);
-    QSize s = fm.size(0, text);
-    
-    QImage image(s + QSize(MARGIN * 2, MARGIN * 2), QImage::Format_RGB32);
-    image.fill(app.palette().color(QPalette::Base));
-    
-    QPainter painter(&image);
 
-    painter.setFont(font);
-    painter.setPen(app.palette().color(QPalette::Text));
-    painter.drawText(QRect(QPoint(MARGIN, MARGIN), s), Qt::AlignCenter, text);
+    PaintWidget widget;
+
+    QImage image(widget.size(), QImage::Format_RGB32);
+    image.fill(app.palette().color(QPalette::Base));
+    widget.render(&image);
     
     QString filename = KStandardDirs::locateLocal("tmp", QString::fromLocal8Bit(argv[1]).append(".png"));
+    qDebug() << filename;
     QImageWriter imageWriter(filename, "PNG");
     if (!imageWriter.canWrite()) {
         kDebug() << "Cannot write PNG format";
@@ -56,3 +75,5 @@ int main(int argc, char* argv[])
     imageWriter.write(image);
     return 0;
 }
+
+#include "main.moc"
